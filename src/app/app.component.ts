4b50e86fdf9fa8af5c31f2477ad3547f6d2a3d15
Nandini P S <nandinipsreddy@gmail.com>
2018-11-22 21:57:15 +0530

    commit after ts lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    alert('kindly enter your api key before proceeding');
+  }
 }
